2357ef78b6ef90789e45892f79f246d7214b518a
Adrien Logut <newsadrien@gmail.com>
2020-06-01 22:44:04 -0400

    Add problem 23

--- Problem23/solution.py
+++ Problem23/solution.py
@@ -1,13 +1,26 @@
 from utils.util import get_divisors
+from collections import Set
 
 abundants = []
 
 value_max = 28123
-value_max = 28300
 
 for i in range(value_max):
     divisors = get_divisors(i)[:-1]
     if sum(divisors) > i:
         abundants.append(i)
 
-print(abundants)
+sum_abundants = set()
+
+for i in range(len(abundants)):
+    for other in abundants[i:]:
+        if abundants[i] + other >= value_max:
+            break
+        sum_abundants.add(abundants[i] + other)
+
+res = 0
+for i in range(value_max):
+    if i not in sum_abundants:
+        res += i
+
+print(res)
