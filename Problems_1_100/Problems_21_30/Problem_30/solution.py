e04bf28679f031f8da3cc1a4fecb7d685c0fa958
Adrien Logut <newsadrien@gmail.com>
2020-12-06 11:32:58 -0500

    Solution 30

---
+++ Problems_1_100/Problems_21_30/Problem_30/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+power = 5
+all_powers = [i**power for i in range(10)]
+
+final_res = []
+
+def get_sum(n: int, powers: List[int]):
+    res = 0
+    while n > 0:
+        res += powers[(n % 10)]
+        n = n // 10
+    return res
+
+for i in range(2, 1000000):
+    if i == get_sum(i, all_fifth_powers):
+        final_res.append(i)
+
+print(sum(final_res))
