a05854db1afe2840b53b2bcb71c37320af3ff2c1
Adrien Logut <newsadrien@gmail.com>
2020-12-06 11:08:29 -0500

    Solution 29

--- Problems_1_100/Problems_21_30/Problem_29/solution.py
+++ Problems_1_100/Problems_21_30/Problem_29/solution.py
@@ -1,1 +1,11 @@
+import time
 
+res = set()
+start = time.perf_counter()
+for a in range(2, 101):
+    for b in range(2, 101):
+        res.add(a ** b)
+end = time.perf_counter()
+
+print(len(res))
+print("Duration:", end - start, "s")
