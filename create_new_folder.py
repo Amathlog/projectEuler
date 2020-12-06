6ba5aede8a079cbd72627311097c1a90c7afd7b5
Adrien Logut <newsadrien@gmail.com>
2020-12-06 11:17:58 -0500

    Fix create folder script + init 30

--- create_new_folder.py
+++ create_new_folder.py
@@ -13,11 +13,20 @@ wanted_number = int(sys.argv[1])
 if wanted_number < 1:
     exit(-1)
 
-range_100 = wanted_number // 100 * 100
-range_10 = range_100 + (wanted_number % 100) // 10 * 10
+number_100 = wanted_number // 100 * 100
+if number_100 == wanted_number:
+    range_100 = (number_100 - 99, number_100)
+else:
+    range_100 = (number_100 + 1, number_100 + 100)
 
-folder_100 = "Problems_" + str(range_100 + 1) + "_" + str(range_100 + 100)
-folder_10 = "Problems_" + str(range_10 + 1) + "_" + str(range_10 + 10)
+number_10 = number_100 + (wanted_number % 100) // 10 * 10
+if wanted_number % 10 == 0:
+    range_10 = (number_10 - 9, number_10)
+else:
+    range_10 = (number_10 + 1, number_10 + 10)
+
+folder_100 = "Problems_" + "_".join(map(str, range_100))
+folder_10 = "Problems_" + "_".join(map(str, range_10))
 folder_1 = "Problem_" + str(wanted_number)
 
 final_path = current_path / folder_100 / folder_10 / folder_1
